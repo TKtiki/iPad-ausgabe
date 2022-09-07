f871aa847166f748921488e2b2fde06698a33eab
Hendrik <reu.hendrik@yahoo.com>
2022-09-07 10:52:13 +0200

    Spring Implementation

---
+++ src/main/java/com/bkwitten/Digitalisierung/DigitalisierungApplication.java
@@ -0,0 +1,13 @@
+package com.bkwitten.Digitalisierung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalisierungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalisierungApplication.class, args);
+	}
+
+}

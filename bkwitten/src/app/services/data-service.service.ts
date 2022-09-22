1eb00ecaa9ca2e5ff68ce260baf67098f797cdb0
Win32Ransome <reu.hendrik@yahoo.com>
2022-09-22 10:39:34 +0200

    Button Design. Spelling mistakes.

--- bkwitten/src/app/services/data-service.service.ts
+++ bkwitten/src/app/services/data-service.service.ts
@@ -1,31 +1,32 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class DataServiceService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  API = 'http://localhost:8080';
+  API = "http://localhost:8080";
 
   public registerStudent(studentData: any) {
-    return this.http.post(this.API + '/registerSchüler', studentData);
+    return this.http.post(this.API + "/registerSchüler", studentData);
   }
 
   public getStudents() {
-    return this.http.get(this.API + '/getSchüler');
+    return this.http.get(this.API + "/getSchüler");
   }
 
   public deleteStudent(id: any) {
-    return this.http.delete(this.API + '/deleteSchüler?id=' + id);
+    return this.http.delete(this.API + "/deleteSchüler?id=" + id);
   }
 
   public updateStudents(student: any) {
-    return this.http.put(this.API + '/updateSchüler', student);
+    return this.http.put(this.API + "/updateSchüler", student);
   }
 
-
+  public getAvailableDevices() {
+    return this.http.get(this.API + "/numberWithStatus");
+  }
 }
